248fd6f9ddf1ed6e835d0dd87a1c014618825983
Dhruv Baldawa <dhruvbaldawa@gmail.com>
2014-04-27 14:57:34 +0530

    gaaah! silly issues

--- themes/custom/assets/js/script.js
+++ themes/custom/assets/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
         $(".easydate").easydate();
     });
 
-    if (!document.referrer.indexOf(window.location.host) > 0) {
+    if (document.referrer.indexOf(window.location.host) < 0) {
         // some issue with jQuery not selecting all the instances, or
         // maybe I am missing something, this seems to work anyways.
         jQuery(document.querySelectorAll('#back-button')).hide();
